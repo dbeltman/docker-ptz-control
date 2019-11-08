695194e45718db0b7be763cc6e647ee7acaef5a3
Daan <daan.w.beltman@gmail.com>
2019-11-08 16:26:48 +0100

    Fork

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN git clone https://github.com/dbeltman/flask-onvif-zeep
 FROM python:3.7-alpine
 WORKDIR /app
 COPY --from=cloner /app/flask-onvif-zeep/requirements.txt  .
+RUN apk add libxml2-dev libxslt-dev
 RUN pip install -r requirements.txt
 COPY --from=cloner /app/flask-onvif-zeep/ .
 
---
+++ main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+import components.presets as presets
+
+app = Starlette(debug=True)
+
+
+@app.route('/')
+async def homepage(request):
+	return JSONResponse({'hello': 'world'})
+
+
+@app.route('/presets')
+async def list_presets(self=None):
+	preset_list = presets.get_presets_by_id(self)
+	return str(preset_list)
+
+
+@app.route("/goto/<preset_id>")
+async def goto_preset(preset_id):
+	result = presets.goto_preset(preset_id)
+	return result
+
+
+if __name__ == '__main__':
+	uvicorn.run(app, host='0.0.0.0', port=5000)
